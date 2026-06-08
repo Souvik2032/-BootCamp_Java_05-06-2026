e2f55a5ec59ee072f2f485c67f90595e5202a0fd
Souvik Layek <souviklayek888@gmail.com>
2026-06-08 07:21:48 +0530

    Update FileInformation.java

--- FileInformation.java
+++ FileInformation.java
@@ -9,7 +9,7 @@ public class FileInformation {
             File obj = new File("C:\\TextFolder_Souvik\\Sou.txt");
 
             if (obj.exists()) {
-                System.out.println("File deleted: " +obj.getName());
+                System.out.println("File name: " +obj.getName());
                 System.out.println("Absolute Path" +obj.getAbsolutePath());
                 System.out.println("Writable: " +obj.canWrite());
                 System.out.println("Readable: " +obj.canRead());
